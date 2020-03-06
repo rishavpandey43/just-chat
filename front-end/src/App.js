fd8fb2e759c3f15c8449acb48330bd9af23c12fe
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-03-06 21:26:17 +0530

    feat: add join and chat component, and route the app using react-router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Chat from "./components/Chat";
+import Join from "./components/Join";
+
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" component={Chat} />
+  </Router>
+);
 
 export default App;
