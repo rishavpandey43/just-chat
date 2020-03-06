fe00501d32c06dd2d4844a3b5bb0621d82e63c98
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-03-07 00:30:08 +0530

    feat: add new event on sendMessage from client side and broadcast the message received from client-side to that room

--- back-end/index.js
+++ back-end/index.js
@@ -4,6 +4,13 @@ const socket = require("socket.io");
 
 const router = require("./routes/router");
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./helperFunctions/users");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -12,10 +19,28 @@ const server = http.createServer(app);
 const io = socket(server);
 
 io.on("connection", socket => {
-  console.log("We have a new connection!!!");
-
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      return callback(error);
+    }
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} has joined`
+    });
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
   });
 
   socket.on("disconnect", () => {
