77057d8a00da468f0b62bcd017f91eb7f845cc64
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-04-09 11:19:37 +0530

    :sparkles: feat: add new option to setting and user short detail in sidebar

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FiUsers, FiLogOut } from "react-icons/fi";
+import { FaEdit, FaAngleRight } from "react-icons/fa";
+import { MdUpdate } from "react-icons/md";
 import { TiMessages } from "react-icons/ti";
 import { AiOutlineSetting, AiOutlineProfile } from "react-icons/ai";
 
 import "./sideBar.css";
 
 const SideBar = (props) => {
+  const [state, setState] = useState({
+    displaySettingOption: false,
+  });
+
   return (
     <aside
       className={`${
@@ -19,31 +27,89 @@ const SideBar = (props) => {
             <GiHamburgerMenu />
           </button>
         </div>
+        <div className="profile-detail">
+          <div className="image">
+            <img
+              src={require("../../assets/images/user.png")}
+              alt="user-name"
+              width="100%"
+            />
+          </div>
+          <div className="name mt-2">
+            <span>Rishav Pandey</span>
+          </div>
+        </div>
         <ul className="sidebar-list">
           <li className="list">
             <span className="icon">
               <AiOutlineProfile />
             </span>
-            <span>Profile</span>
+            <span className="content">
+              <Link to="/">Profile</Link>
+            </span>
           </li>
           <li className="list">
             <span className="icon">
               <FiUsers />
             </span>
-            <span>Friends</span>
+            <span className="content">
+              <Link to="/">Friends</Link>
+            </span>
           </li>
           <li className="list">
             <span className="icon">
               <TiMessages />
             </span>
-            <span>Messages</span>
+            <span className="content">
+              <Link to="/">Messages</Link>
+            </span>
           </li>
-          <li className="list">
+          <li
+            className="list"
+            onClick={() => {
+              let tempState = { ...state };
+              tempState.displaySettingOption = tempState.displaySettingOption
+                ? false
+                : true;
+              setState(tempState);
+            }}
+          >
             <span className="icon">
               <AiOutlineSetting />
             </span>
-            <span>Setting</span>
+            <span className="content">
+              <Link to="/">Setting</Link>
+            </span>
+            <span className="pl-4">
+              <FaAngleRight
+                className={`${state.displaySettingOption ? "rotate-90" : ""}`}
+              />
+            </span>
           </li>
+          <div
+            className={`setting-option mt-3 ${
+              state.displaySettingOption ? "d-block" : "d-none"
+            }`}
+          >
+            <ul className="sidebar-list">
+              <li className="list">
+                <span className="icon">
+                  <FaEdit />
+                </span>
+                <span className="content">
+                  <Link to="/">Change Password</Link>
+                </span>
+              </li>
+              <li className="list">
+                <span className="icon">
+                  <MdUpdate />
+                </span>
+                <span className="content">
+                  <Link to="/">Update Profile</Link>
+                </span>
+              </li>
+            </ul>
+          </div>
         </ul>
         <div className="logout">
           <button onClick={props.logoutFetch} className="logout-btn btn">
