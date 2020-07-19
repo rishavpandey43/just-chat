6d23dd8d8255e782243b0d8596f9bea20d9818f8
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2020-07-19 22:36:57 +0530

    :rocket: change backend base-url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = 'https://chat-box-backend.herokuapp.com';
+// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://chat-box-backend.herokuapp.com';
 
 export const storageAuthTokenName = 'just_chat_auth_token';
 export const storageRefreshTokenName = 'just_chat_refresh_token';
